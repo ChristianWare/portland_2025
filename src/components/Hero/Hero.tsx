3eafbebf2e9c1ffac128d110533217e1d22b8de6
Chris Ware <c.p.ware@hotmail.com>
2024-12-20 16:19:29 -0700

    h1 fix

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -3,6 +3,9 @@ import styles from "./Hero.module.css";
 const Hero = () => {
   return (
     <section className={styles.container}>
+      <p className={styles.copy}>
+        Next.js & React Developer
+      </p>
       <h1 className={styles.heading}>CHRISTIAN WARE</h1>
     </section>
   );
